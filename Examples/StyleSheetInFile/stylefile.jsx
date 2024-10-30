f8f03a00fbe998b7d7fa456d211a760c123123fc
MilesHoffman <129900515+MilesHoffman@users.noreply.github.com>
2024-10-30 10:42:50 -0400

    Added example of a stylesheet within a file

---
+++ Examples/StyleSheetInFile/stylefile.jsx
@@ -0,0 +1,26 @@
+import {Tabs} from "expo-router";
+import {Text, StyleSheet} from "react-native";
+import {Button, useTheme} from "react-native-paper";
+import {router} from 'expo-router'
+import {BackDrop} from "../../../components/Backdrop.jsx";
+
+
+export default function InventoryPage(){
+	return(
+
+		<BackDrop style={styleExample}>
+			<Text>This is the Inventory page</Text>
+			<Button style={{ backgroundColor: 'green'}} onPress={ () => router.navigate('inventoryTab/stackPage') }>
+				TestingPagee
+			</Button>
+		</BackDrop>
+	)
+}
+
+
+
+const styleExample = StyleSheet.create({
+	alignItems: 'center',
+	justifyContent: 'center'
+})
+
