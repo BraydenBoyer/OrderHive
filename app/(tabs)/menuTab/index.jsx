6d1ee0a061762be11762b9016cadb3074aa9873b
Brayden <braydenboyer@yahoo.com>
2024-10-30 23:27:28 -0400

    progress the menu screen UI, organized button layout and text color

--- app/(tabs)/menuTab/index.jsx
+++ app/(tabs)/menuTab/index.jsx
@@ -1,5 +1,5 @@
 import {BackDrop} from "../../../components/Backdrop.jsx";
-import {Text,View} from "react-native";
+import {Text,View,StyleSheet} from "react-native";
 import {Button} from "react-native-paper";
 import {router} from "expo-router";
 
@@ -8,29 +8,29 @@ export default function MenuPage() {
 	return (
 
 		<BackDrop>
-			<View style={{alignItems: "center",flexDirection: 'column', justifyContent: 'center',flex: 1}}>
-				<View style={{alignContent: 'center'}}>
-					<Button onPress={ () => router.navigate('menuTab/userDetails')} style={{backgroundColor: "red",borderRadius: 7
-						, width: 300,height: 50}}>User Details</Button>
+			<View style={{alignItems: "center",flexDirection: 'column', justifyContent: 'center',flex: 1,}}>
+				<View style={{alignContent: 'center', paddingBottom: 5}}>
+					<Button onPress={ () => router.navigate('menuTab/userDetails')} style={{backgroundColor: "#353562",borderRadius: 7
+						, width: 300,height: 50,justifyContent: 'center', borderBottomLeftRadius:0,borderBottomRightRadius:0}}><Text style={styles.text}>User Details</Text></Button>
 
-					<Button onPress={ () => router.navigate('menuTab/CompanyDetails')} style={{backgroundColor: "yellow",borderRadius: 7
-						, width: 300,height: 50}}>Company Details</Button>
+					<Button onPress={ () => router.navigate('menuTab/CompanyDetails')} style={{backgroundColor: "#353562",borderRadius: 7
+						, width: 300,height: 50, justifyContent: 'center',borderTopLeftRadius: 0,borderTopRightRadius: 0}}><Text style={styles.text}>Company Details</Text></Button>
 				</View>
 
-				<View style={{alignContent: 'center'}}>
-					<Button onPress={ () => router.navigate('menuTab/Collaborators')} style={{backgroundColor: "blue",borderRadius: 7
-						, width: 300,height: 50}}>Collaborators</Button>
+				<View style={{alignContent: 'center',paddingBottom: 5}}>
+					<Button onPress={ () => router.navigate('menuTab/Collaborators')} style={{backgroundColor: "#353562",borderRadius: 7
+						, width: 300,height: 50, justifyContent: 'center'}}> <Text style={styles.text}>Collaborators</Text> </Button>
 				</View>
 
 
-				<View style={{alignContent: 'center'}}>
-					<Button onPress={ () => router.navigate('menuTab/SubscriptionPlan')} style={{backgroundColor: "gray",borderRadius: 7
-						, width: 300,height: 50}}>Subscription Plan</Button>
+				<View style={{alignContent: 'center',paddingBottom: 5}}>
+					<Button onPress={ () => router.navigate('menuTab/SubscriptionPlan')} style={{backgroundColor: "#353562",borderRadius: 7
+						, width: 300,height: 50, justifyContent: 'center '}}><Text style={styles.text}>Subscription Plan</Text></Button>
 				</View>
 
-				<View style={{alignContent: 'center'}}>
-					<Button onPress={ () => router.navigate('menuTab/Appearance')} style={{backgroundColor: "green",borderRadius: 7
-						, width: 300,height: 50,justifyContent: 'center'}}>Appearance</Button>
+				<View style={{alignContent: 'center',paddingBottom: 5}}>
+					<Button onPress={ () => router.navigate('menuTab/Appearance')} style={{backgroundColor: "#353562",borderRadius: 7
+						, width: 300,height: 50,justifyContent: 'center'} }><Text style={styles.text}>Appearance</Text></Button>
 				</View>
 
 			</View>
@@ -40,9 +40,14 @@ export default function MenuPage() {
 }
 
 
-/*
+
 const styles = StyleSheet.create({
-	alignItems: 'center',
-	justifyContent: 'center',
-})
-*/
+
+	text: {
+		fontSize: 16,
+		lineHeight: 21,
+		fontWeight: 'bold',
+		letterSpacing: 0.25,
+		color: 'white',
+	},
+});
