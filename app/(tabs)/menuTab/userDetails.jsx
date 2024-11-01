07abce063f9850eec846f34f2ad76b0ee3c88593
Brayden <braydenboyer@yahoo.com>
2024-11-01 02:57:56 -0400

    Set up majority of User Details Layout, also added functionality for user enetering text and being able to save and edit in the text fields with a button

--- app/(tabs)/menuTab/userDetails.jsx
+++ app/(tabs)/menuTab/userDetails.jsx
@@ -1,5 +1,5 @@
 import {Link, Tabs} from "expo-router";
-import {Button, StyleSheet, Text, View} from "react-native";
+import {Button, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import {router} from 'expo-router'
 import {BackDrop} from "../../../components/Backdrop.jsx";
 import React,{useState} from "react";
@@ -7,30 +7,103 @@ import {TextInput} from "react-native-paper";
 
 
 export default function MenuPage() {
-    const [text, setText] = React.useState("");
-    const [isEditable, setIsEditable] = useState(false);
-    const handleEditToggle = () => {
-        setIsEditable(!isEditable);
+    const [textUN, setTextUN] = React.useState("");
+    const [textPass, setTextPass] = React.useState("");
+    const [textEm, setTextEm] = React.useState("");
+    const [textPM, setTextPM] = React.useState("");
+
+    const [isEditableUN, setIsEditableUN] = useState(false);
+    const [isEditablePass, setIsEditablePass] = useState(false);
+    const [isEditableEm, setIsEditableEm] = useState(false);
+    const [isEditablePN, setIsEditablePN] = useState(false);
+
+    const handleEditToggleUn = () => {
+        setIsEditableUN(!isEditableUN);
+    };
+
+    const handleEditTogglePass = () => {
+        setIsEditablePass(!isEditablePass);
+    };
+
+    const handleEditToggleEm = () => {
+        setIsEditableEm(!isEditableEm);
+    };
+
+    const handleEditTogglePn = () => {
+        setIsEditablePN(!isEditablePN);
     };
     return (
         <BackDrop>
             <View>
                 <Text style={styles.title}>User Details</Text>
             </View>
-            <View>
+            <View style={styles.container}>
 
-                <View style={styles.container}>
-                    <TextInput
-                        style={styles.textInput}
-                        mode="outlined"
-                        label="Email"
-                        value={text}
-                        onChangeText={text => setText(text)}
-                        editable={isEditable}
+                <View style={[styles.backgroundContainer,
+                    {borderBottomLeftRadius: 0,borderBottomRightRadius: 0,borderBottomWidth:.5,borderBottomColor:'white'}]}>
+                    <Text style={styles.text}>UserName</Text>
+                    <View style={styles.inputContainer}>
+                        <TextInput
+                            style={{width:'80%',height: 30}}
+                            mode="outlined"
+                            value={textUN}
+                            onChangeText={textUN => setTextUN(textUN)}
+                            editable={isEditableUN}
 
-                    />
-                    <Button title={isEditable ? "Save" : "Edit"} onPress={handleEditToggle} />
+                        />
+                        <Button title={isEditableUN ? "Save" : "Edit"} onPress={handleEditToggleUn} />
+                    </View>
                 </View>
+
+                <View style={[styles.backgroundContainer,
+                    {borderRadius: 0,borderBottomWidth:.5,borderBottomColor:'white'}]}>
+                    <Text style={styles.text}>Password</Text>
+                    <View style={styles.inputContainer}>
+                        <TextInput
+                            style={{width:'80%',height: 30}}
+                            mode="outlined"
+                            value={textPass}
+                            onChangeText={textPass => setTextPass(textPass)}
+                            editable={isEditablePass}
+                            secureTextEntry
+                            right={<TextInput.Icon icon="eye" />}
+
+                        />
+                        <Button title={isEditablePass ? "Save" : "Edit"} onPress={handleEditTogglePass} />
+                    </View>
+                </View>
+
+                <View style={[styles.backgroundContainer,
+                    {borderRadius: 0,borderBottomWidth:.5,borderBottomColor:'white'}]}>
+                    <Text style={styles.text}>Email</Text>
+                    <View style={styles.inputContainer}>
+                        <TextInput
+                            style={{width:'80%',height: 30}}
+                            mode="outlined"
+                            value={textEm}
+                            onChangeText={textEm => setTextEm(textEm)}
+                            editable={isEditableEm}
+
+                        />
+                        <Button title={isEditableEm ? "Save" : "Edit"} onPress={handleEditToggleEm} />
+                    </View>
+                </View>
+
+                <View style={[styles.backgroundContainer, {borderTopLeftRadius: 0,borderTopRightRadius: 0}]}>
+                    <Text style={styles.text}>Phone</Text>
+                    <View style={styles.inputContainer}>
+                        <TextInput
+                            style={{width:'80%',height: 30}}
+                            mode="outlined"
+                            value={textPM}
+                            onChangeText={textPM => setTextPM(textPM)}
+                            editable={isEditablePN}
+
+                        />
+                        <Button title={isEditablePN ? "Save" : "Edit"} onPress={handleEditTogglePn} />
+                    </View>
+                </View>
+
 
 
 
@@ -44,7 +117,7 @@ export default function MenuPage() {
 const styles = StyleSheet.create({
 
     text: {
-        fontSize: 16,
+        fontSize: 20,
         lineHeight: 21,
         fontWeight: 'bold',
         letterSpacing: 0.25,
@@ -56,18 +129,42 @@ const styles = StyleSheet.create({
         textAlign: 'left',
     },
 
+
     container: {
-        flexDirection: 'row', // Align items horizontally
         alignItems: 'center',
         paddingHorizontal: 10,
-        backgroundColor: '#353562'
+        flexDirection: 'column',
+    },
+
+    backgroundContainer: {
+        flexDirection: 'column',
+        backgroundColor: '#353562',
+        padding: 10,
+        borderRadius: 10,
+        marginRight: 10,
+        height: 100, // Set desired container height
+        width:388,
+        alignItems:"left",
+
+    },
+    inputContainer: {
+        flexDirection: 'row',
+
+        height: 100, // Set desired container height
+        width:388,
+        alignItems:"center",
+        paddingTop: 10,
+        paddingBottom: 40,
 
     },
     textInput: {
         maxWidth:'85%',
-        height: 45,
+        height: 10,
         flex: 1, // Take up available space
-        marginRight: 10, // Add space between TextInput and Button
+
+        paddingHorizontal: 50,
+        paddingRight: 20
+
 
     },
 });
