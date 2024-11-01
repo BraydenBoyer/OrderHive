ef1be4b6086e0e3dfebddba2b3f38f53d9967b16
Brayden <braydenboyer@yahoo.com>
2024-11-01 01:58:40 -0400

    Almost Finished with main menu tab layout. Moved on to starting user details view, started working on setting up the user input

--- app/(tabs)/menuTab/index.jsx
+++ app/(tabs)/menuTab/index.jsx
@@ -2,35 +2,40 @@ import {BackDrop} from "../../../components/Backdrop.jsx";
 import {Text,View,StyleSheet} from "react-native";
 import {Button} from "react-native-paper";
 import {router} from "expo-router";
+import React from "react";
 
 export default function MenuPage() {
 
 	return (
 
 		<BackDrop>
-			<View style={{alignItems: "center",flexDirection: 'column', justifyContent: 'center',flex: 1,}}>
-				<View style={{alignContent: 'center', paddingBottom: 5}}>
-					<Button onPress={ () => router.navigate('menuTab/userDetails')} style={{backgroundColor: "#353562",borderRadius: 7
-						, width: 300,height: 50,justifyContent: 'center', borderBottomLeftRadius:0,borderBottomRightRadius:0}}><Text style={styles.text}>User Details</Text></Button>
+			<View>
+				<Text style={styles.title}>Menu</Text>
+			</View>
 
-					<Button onPress={ () => router.navigate('menuTab/CompanyDetails')} style={{backgroundColor: "#353562",borderRadius: 7
-						, width: 300,height: 50, justifyContent: 'center',borderTopLeftRadius: 0,borderTopRightRadius: 0}}><Text style={styles.text}>Company Details</Text></Button>
+			<View style={{alignItems: "center",flexDirection: 'column', justifyContent: 'center',flex: 1}}>
+				<View style={{alignContent: 'center',paddingTop: 5}}>
+					<Button onPress={ () => router.navigate('menuTab/userDetails')} style={{backgroundColor:'#353562',paddingHorizontal: 15, borderRadius: 8, alignItems: 'center',
+						opacity: 1,height: 75,width: 375,borderBottomLeftRadius: 0,borderBottomRightRadius: 0,borderBottomWidth:.5,borderBottomColor:'white',paddingVertical:20}}>
+						<Text style={styles.text}>User Details</Text>
+					</Button>
+
+					<Button onPress={ () => router.navigate('menuTab/CompanyDetails')}style={{backgroundColor:'#353562',paddingHorizontal: 15, borderRadius: 8, alignItems: 'center',
+						opacity: 1,height: 75,width: 375,borderTopLeftRadius: 0,borderTopRightRadius: 0,paddingVertical:20,marginBottom:20}}><Text style={styles.text}>Company Details</Text></Button>
 				</View>
 
-				<View style={{alignContent: 'center',paddingBottom: 5}}>
-					<Button onPress={ () => router.navigate('menuTab/Collaborators')} style={{backgroundColor: "#353562",borderRadius: 7
-						, width: 300,height: 50, justifyContent: 'center'}}> <Text style={styles.text}>Collaborators</Text> </Button>
+				<View style={{alignContent: 'center'}}>
+					<Button onPress={ () => router.navigate('menuTab/Collaborators')} style={styles.button }>
+						<Text style={styles.text}>Collaborators</Text> </Button>
 				</View>
 
 
-				<View style={{alignContent: 'center',paddingBottom: 5}}>
-					<Button onPress={ () => router.navigate('menuTab/SubscriptionPlan')} style={{backgroundColor: "#353562",borderRadius: 7
-						, width: 300,height: 50, justifyContent: 'center '}}><Text style={styles.text}>Subscription Plan</Text></Button>
+				<View style={{alignContent: 'center'}}>
+					<Button onPress={ () => router.navigate('menuTab/SubscriptionPlan')} style={styles.button }><Text style={styles.text}>Subscription Plan</Text></Button>
 				</View>
 
-				<View style={{alignContent: 'center',paddingBottom: 5}}>
-					<Button onPress={ () => router.navigate('menuTab/Appearance')} style={{backgroundColor: "#353562",borderRadius: 7
-						, width: 300,height: 50,justifyContent: 'center'} }><Text style={styles.text}>Appearance</Text></Button>
+				<View style={{alignContent: 'center'}}>
+					<Button onPress={ () => router.navigate('menuTab/Appearance')} style={styles.button }><Text style={styles.text}>Appearance</Text></Button>
 				</View>
 
 			</View>
@@ -50,4 +55,21 @@ const styles = StyleSheet.create({
 		letterSpacing: 0.25,
 		color: 'white',
 	},
+	title: {
+		fontSize: 45,
+		fontWeight: 'bold',
+		textAlign: 'left',
+	},
+
+	button: {
+		marginBottom: 20,
+		paddingHorizontal: 15,
+		paddingVertical: 20,
+		backgroundColor: '#353562',
+		borderRadius: 8,
+		alignItems: 'center',
+		opacity: 1,
+		height: 75,
+		width: 375
+	}
 });
--- app/(tabs)/menuTab/userDetails.jsx
+++ app/(tabs)/menuTab/userDetails.jsx
@@ -1,17 +1,73 @@
 import {Link, Tabs} from "expo-router";
-import {Button, Text, View} from "react-native";
+import {Button, StyleSheet, Text, View} from "react-native";
 import {router} from 'expo-router'
 import {BackDrop} from "../../../components/Backdrop.jsx";
+import React,{useState} from "react";
+import {TextInput} from "react-native-paper";
 
 
 export default function MenuPage() {
+    const [text, setText] = React.useState("");
+    const [isEditable, setIsEditable] = useState(false);
+    const handleEditToggle = () => {
+        setIsEditable(!isEditable);
+    };
     return (
+        <BackDrop>
+            <View>
+                <Text style={styles.title}>User Details</Text>
+            </View>
+            <View>
 
-        <BackDrop style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text>This is the User Details page. Purely for testing navigation.</Text>
-            <Button title={'Menu'} onPress={() => router.navigate('(tabs)/menuTab')}/>
-            <Link href={'menuTab/index'}/>
+                <View style={styles.container}>
+                    <TextInput
+                        style={styles.textInput}
+                        mode="outlined"
+                        label="Email"
+                        value={text}
+                        onChangeText={text => setText(text)}
+                        editable={isEditable}
+
+                    />
+                    <Button title={isEditable ? "Save" : "Edit"} onPress={handleEditToggle} />
+                </View>
+
+
+
+            </View>
+
         </BackDrop>
     )
 }
 
+
+const styles = StyleSheet.create({
+
+    text: {
+        fontSize: 16,
+        lineHeight: 21,
+        fontWeight: 'bold',
+        letterSpacing: 0.25,
+        color: 'white',
+    },
+    title: {
+        fontSize: 45,
+        fontWeight: 'bold',
+        textAlign: 'left',
+    },
+
+    container: {
+        flexDirection: 'row', // Align items horizontally
+        alignItems: 'center',
+        paddingHorizontal: 10,
+        backgroundColor: '#353562'
+
+    },
+    textInput: {
+        maxWidth:'85%',
+        height: 45,
+        flex: 1, // Take up available space
+        marginRight: 10, // Add space between TextInput and Button
+
+    },
+});
